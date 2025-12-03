446d890a08c4d478877e0c306bd67695eb11cfac
emilkoooFD <bukharover123@mail.ru>
2025-12-03 16:19:41 +0300

    Удалена папка draft_go из репозитория (оставлена локально)

--- draft_go/main.go
+++ draft_go/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// "math"
+
 func main() {
-	fmt.Println("Hello, world!")
+	// math.Pow(x, y) - метод возводящий в степень (x - число возводимое в степень, y - степень в которую возводим)
+	const USD_RUB = 90.0
+	const USD_EUR = 1.1
+
+	EURInRUB := USD_RUB * USD_EUR
+	fmt.Println(EURInRUB)
+
 }
--- practice_go/pract-1/main.go
+++ practice_go/pract-1/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+)
 
+// Калькулятор ИМТ
+func main() {
+	var userHeight float64
+	var userWeight float64
+	fmt.Println("__ Калькулятор ИТМ __")
+	fmt.Println("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Println("Введите свой вес: ")
+	fmt.Scan(&userWeight)
+	IMT := userWeight / math.Pow(userHeight, 2)
+	fmt.Println("Ваш ИМТ =", IMT)
 }
